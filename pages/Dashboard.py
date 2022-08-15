cdfdf28ff16f2b1303507a0818945c04b1156989
Stanislav Horkusha <hork17hs@gmail.com>
2022-08-15 19:46:15 +0300

    slight changes to xpaths, thanks for office hours!

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -11,15 +11,18 @@ class Dashboard(BasePage):
     # since it will allow us to access this button on both versions
     # but text locators is shorter and it's unclear after a lesson
     # which locator is optimal in this case, I would prefer aforementioned universal xpath
+    #
+    # UPD: changed and cropped xpaths after watching Office hours, thanks Pati <3
 
-    main_page_button_xpath = "//*[text()='Main page']"
-    players_button_xpath = "//*[text()='Players']"
-    polski_button_xpath = "//*[text()='Polski']"
-    sign_out_button_xpath = "//*[text()='Sign out']"
+    main_page_button_xpath = "//*/ul[1]/div[1]/div[2]/span"
+    players_button_xpath = "//*/ul[1]/div[2]/div[2]/span"
+    polski_button_xpath = "//*/ul[2]/div[1]/div[2]/span"
+    sign_out_button_xpath = "//*/ul[2]/div[2]/div[2]/span"
     sidebar_burger_menu_button_xpath = "//*[@aria-label='menu']"
-    dev_team_contact_button_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[1]/div/div[3]/a"
-    add_player_button_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[2]/div/div/a/button/span[1]"
-    last_created_player_button_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[1]/button/span[1]"
-    last_updated_player_button_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[2]/button/span[1]"
-    last_updated_report_button_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[3]/button/span[1]"
+    dev_team_contact_button_xpath = "//*/div/div[3]/a"
+    add_player_button_xpath = "//*/div[2]/div/div/a/button/span[1]"
+    last_created_player_button_xpath = "//*/div[3]/div/div/a[1]/button/span[1]"
+    last_updated_player_button_xpath = "//*/div[3]/div/div/a[2]/button/span[1]"
+    last_updated_report_button_xpath = "//*/div[3]/div/div/a[3]/button/span[1]"
+    logo_scouts_panel_xpath = "//*[contains(@class,'MuiCardMedia-root')]"
     pass
