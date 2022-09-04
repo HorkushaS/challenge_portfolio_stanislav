5dbaee02680a8bab4df0a53eb9b1f1503b0e83a1
Stanislav Horkusha <105933612+HorkushaS@users.noreply.github.com>
2022-09-04 19:21:44 +0300

    Task 5
    Added all_suite_test.py
    Gathered all TCs in the login_to_the_system.py
    Removed redundant files
    Switched from macOs to Windows for this tasks so chromedriver and some files were changed to match new OS

--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -29,6 +29,86 @@ class TestLoginPage(unittest.TestCase):
         dashboard_page = Dashboard(self.driver)
         dashboard_page.title_of_page()
 
+    def test_log_out_from_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.assert_element_text(self.driver, LoginPage.title_of_box_xpath, LoginPage.header_of_box)
+        user_login.type_in_email('user02@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.wait_for_element_to_be_clickable(user_login.sign_in_button_xpath)
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        dashboard_page.click_on_the_log_out_button()
+        user_login.wait_for_element_to_be_visible(user_login.sign_in_button_xpath)
+        user_login.assert_element_text(self.driver, LoginPage.title_of_box_xpath, LoginPage.header_of_box)
+
+
+    def test_log_in_to_the_system_negative_password(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.assert_element_text(self.driver, LoginPage.title_of_box_xpath, LoginPage.header_of_box)
+        user_login.type_in_email('user02@getnada.com')
+        user_login.type_in_password('test-1234')
+        user_login.wait_for_element_to_be_clickable(user_login.sign_in_button_xpath)
+        user_login.click_on_the_sign_in_button()
+        user_login.wait_for_element_to_be_visible(user_login.password_warning_xpath)
+        user_login.assert_element_text(self.driver, LoginPage.password_warning_xpath, LoginPage.password_warning)
+
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+
+class TestFillInaForm(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_add_player(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.assert_element_text(self.driver, LoginPage.title_of_box_xpath, LoginPage.header_of_box)
+        user_login.type_in_email('user02@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.wait_for_element_to_be_clickable(user_login.sign_in_button_xpath)
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        dashboard_page.click_on_the_add_player_button()
+        dashboard_page.wait_for_element_to_be_visible(dashboard_page.add_player_span_xpath)
+        dashboard_page.type_in_name(dashboard_page.name)
+        dashboard_page.type_in_surname(dashboard_page.surname)
+        dashboard_page.type_in_age(dashboard_page.age)
+        dashboard_page.type_in_main_position(dashboard_page.main_position)
+        dashboard_page.click_on_the_submit_button()
+        time.sleep(3)
+
+    def test_add_player_verification(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.assert_element_text(self.driver, LoginPage.title_of_box_xpath, LoginPage.header_of_box)
+        user_login.type_in_email('user02@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.wait_for_element_to_be_clickable(user_login.sign_in_button_xpath)
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        dashboard_page.click_on_the_players_button()
+        dashboard_page.wait_for_element_to_be_clickable(dashboard_page.filter_table_button_xpath)
+        dashboard_page.click_on_the_filter_table_button()
+        dashboard_page.type_in_filter_name(dashboard_page.name)
+        dashboard_page.type_in_filter_surname(dashboard_page.surname)
+        dashboard_page.wait_for_element_to_be_visible(dashboard_page.filter_table_main_position_xpath)
+        dashboard_page.assert_main_position()
+        time.sleep(3)
 
     @classmethod
     def tearDown(self):
